bffb0c15ac32a6800fe28787cb0942f2e6ed3540
henryuniuni <ak47m245@gmail.com>
2020-03-23 21:03:16 +0800

    Replace rmJSCSS with rmTag and implenment fetching urls

--- crawler.cpp
+++ crawler.cpp
@@ -46,60 +46,65 @@ void write()
     output_file << "Content : " << std::endl << m_pBuffer << std::endl;
 }
 
-/* String process */
-void rmJSCSS(){
+/* remove from <tag> to </tag>*/
+void rmTag(char *tag)
+{
   /* replace newline with  space */
   for (int i=0; i<strlen(m_pBuffer); i++)
     if(m_pBuffer[i]=='\n')
       m_pBuffer[i] = ' ';
   
-  /* remove from <script> to </script>*/
+  char front_tag[20], back_tag[20];
+  sprintf(front_tag, "<%s", tag);   // create <tag for strstr
+  sprintf(back_tag, "</%s>", tag);  // create </tag> for strstr
   char *front_ptr = m_pBuffer, *back_ptr=NULL;
   int time=0;
-  while((front_ptr = strstr(front_ptr, "<script"))!=NULL)
+  while((front_ptr = strstr(front_ptr, front_tag))!=NULL)
   {
     std::cout << time++ << std::endl;
     char *tmp = front_ptr;
     front_ptr++;
     *tmp = '\0';
-    if ((back_ptr = strstr(front_ptr, "</script>"))==NULL) {
+    if ((back_ptr = strstr(front_ptr, back_tag))==NULL) {
       //error
+      std::cout << "error" << std::endl;
       break;
     }
     std::cout << time++ << std::endl;
-    strcat(tmp, back_ptr+8);
-    // std::cout << front_ptr << std::endl;
-    // std::cout << back_ptr << std::endl;
+    strcat(tmp, back_ptr+strlen(back_tag));
+    front_ptr--;
   }
-  std::cout << m_pBuffer << std::endl;
-  /*for (int len=0; ; len++) 
+  // std::cout << m_pBuffer << std::endl;
+}
+
+void retrieveUrl()
+{
+  char *href = m_pBuffer, url_link[128], *tmp;
+  std::ofstream url_file;
+  url_file.open("url.txt");
+  while((href=strstr(href, "href=\"http"))!=NULL)
   {
-    if (len==0) {
-      if ((front_ptr = strstr(m_pBuffer, "script"))==NULL) 
+    tmp = href = href+strlen("href=\"");
+    while(1)
+    {
+      if (*tmp=='\"') {
+        *tmp = '\0';
         break;
+      }
+      tmp++;
     }
-    else
-      if ((front_ptr = strstr(front_ptr, "script"))==NULL) 
-        break;
-    std::cout << "hehe" << std::endl;
-    while(strncmp(front_ptr, "</script>", 8)!=0){
-      std::cout << *front_ptr++;
-    }
+    strcpy(url_link, href);
+    *tmp = ' ';
+    //std::cout << url_link << std::endl;
+    url_file << url_link << std::endl;
   }
-  while ((front_ptr = strstr(m_pBuffer, "<script>"))!=NULL) {
-    int len=0;
-    while(strncmp(front_ptr, "</script>", 8)!=0){
-      len++;
-      front_ptr++;
-      std::cout << *front_ptr;
-    }
-  }*/
+  url_file.close();
 }
 
 int main(int, char **) /* I/O for save data, using dataa batch to control I/O count */
 {
     m_pBuffer = (char*) malloc(MAX_FILE_LENGTH * sizeof(char));
-    output_file.open("test.txt");
+    //output_file.open("test.txt");
     try
     {
         // initial process
@@ -127,11 +132,15 @@ int main(int, char **) /* I/O for save data, using dataa batch to control I/O co
 	{
 		std::cout << e.what() << std::endl;
 	}
-    std::cout << "rmJSCSS()" << std::endl;
-    rmJSCSS();
+    std::cout << "rm script" << std::endl;
+    rmTag("script");
+    std::cout << "rm style" << std::endl;
+    rmTag("style");
+    std::cout << "retrieve URL" << std::endl;
+    retrieveUrl();
     std::cout << "write()" << std::endl;
     //write();
-    output_file.close();
+    //output_file.close();
     free(m_pBuffer);
     return 0;
 }
